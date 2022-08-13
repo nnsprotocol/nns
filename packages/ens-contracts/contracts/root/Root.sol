bbb3f6cf73029e4b09253847cf98bfed741ac6f2
apbigcod <apbigcod@proton.me>
2022-08-13 12:32:45 +0200

    reverted formatting of contracts

--- packages/ens-contracts/contracts/root/Root.sol
+++ packages/ens-contracts/contracts/root/Root.sol
@@ -1,43 +1,46 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import '../registry/ENS.sol';
-import '@openzeppelin/contracts/access/Ownable.sol';
-import './Controllable.sol';
+import "../registry/ENS.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Controllable.sol";
 
 contract Root is Ownable, Controllable {
-  bytes32 private constant ROOT_NODE = bytes32(0);
+    bytes32 private constant ROOT_NODE = bytes32(0);
 
-  bytes4 private constant INTERFACE_META_ID =
-    bytes4(keccak256('supportsInterface(bytes4)'));
+    bytes4 private constant INTERFACE_META_ID =
+        bytes4(keccak256("supportsInterface(bytes4)"));
 
-  event TLDLocked(bytes32 indexed label);
+    event TLDLocked(bytes32 indexed label);
 
-  ENS public ens;
-  mapping(bytes32 => bool) public locked;
+    ENS public ens;
+    mapping(bytes32 => bool) public locked;
 
-  constructor(ENS _ens) public {
-    ens = _ens;
-  }
+    constructor(ENS _ens) public {
+        ens = _ens;
+    }
 
-  function setSubnodeOwner(bytes32 label, address owner)
-    external
-    onlyController
-  {
-    require(!locked[label]);
-    ens.setSubnodeOwner(ROOT_NODE, label, owner);
-  }
+    function setSubnodeOwner(bytes32 label, address owner)
+        external
+        onlyController
+    {
+        require(!locked[label]);
+        ens.setSubnodeOwner(ROOT_NODE, label, owner);
+    }
 
-  function setResolver(address resolver) external onlyOwner {
-    ens.setResolver(ROOT_NODE, resolver);
-  }
+    function setResolver(address resolver) external onlyOwner {
+        ens.setResolver(ROOT_NODE, resolver);
+    }
 
-  function lock(bytes32 label) external onlyOwner {
-    emit TLDLocked(label);
-    locked[label] = true;
-  }
+    function lock(bytes32 label) external onlyOwner {
+        emit TLDLocked(label);
+        locked[label] = true;
+    }
 
-  function supportsInterface(bytes4 interfaceID) external pure returns (bool) {
-    return interfaceID == INTERFACE_META_ID;
-  }
+    function supportsInterface(bytes4 interfaceID)
+        external
+        pure
+        returns (bool)
+    {
+        return interfaceID == INTERFACE_META_ID;
+    }
 }
