48709441644e09cd4f0bef7420fc9b9052feda28
apbigcod <apbigcod@proton.me>
2023-01-29 19:06:29 +0100

    first suboptimal implementation

--- packages/contracts/contracts/ethregistrar/TenKClubController.sol
+++ packages/contracts/contracts/ethregistrar/TenKClubController.sol
@@ -1,19 +1,197 @@
 pragma solidity >=0.8.4;
 
 import '@openzeppelin/contracts/access/Ownable.sol';
+import '../resolvers/Resolver.sol';
 import './BaseRegistrarImplementation.sol';
 
+// import 'hardhat/console.sol';
+
+// TenKClubController registers a random name from 00000 to 99999
+// for accounts that have one or more claims available.
 contract TenKClubController is Ownable {
-  struct Claim {
-    uint64 ttl;
-  }
+  event NameRegistered(
+    string name,
+    bytes32 indexed label,
+    address indexed owner
+  );
 
-  mapping(address => Claim[]) public claims;
+  bytes16 private constant SYMBOLS = '0123456789';
+
+  mapping(uint256 => bool) public denyList;
+  // Map of expiry timestamps for claims associated to addresses.
+  mapping(address => uint256[]) private claims;
   BaseRegistrarImplementation base;
+  uint256 private maxDigits;
 
-  constructor(BaseRegistrarImplementation _base) {
+  constructor(BaseRegistrarImplementation _base, uint256 _maxDigits) {
     base = _base;
+    maxDigits = _maxDigits;
   }
 
-  function register() public {}
+  /**
+    Registration
+  */
+
+  // Register a random name from 0000 to 9999.
+  function register(address resolver, address addr) external {
+    uint256 attempt = 0;
+    string memory name;
+    bytes32 tokenId;
+    do {
+      name = _generateName(attempt);
+      tokenId = keccak256(bytes(name));
+      attempt++;
+    } while (!_available(name));
+
+    _register(name, resolver, addr);
+  }
+
+  // Register the given name.
+  function _register(
+    string memory name,
+    address resolver,
+    address addr
+  ) private {
+    require(_available(name));
+    _consumeClaim();
+
+    bytes32 label = keccak256(bytes(name));
+    uint256 tokenId = uint256(label);
+
+    // Copied from NNSRegistrarControllerWithReservation.sol.
+    if (resolver != address(0)) {
+      // Set this contract as the (temporary) owner, giving it
+      // permission to set up the resolver.
+      base.register(tokenId, address(this));
+
+      // The nodehash of this label
+      bytes32 nodehash = keccak256(abi.encodePacked(base.baseNode(), label));
+
+      // Set the resolver
+      base.ens().setResolver(nodehash, resolver);
+
+      // Configure the resolver
+      if (addr != address(0)) {
+        Resolver(resolver).setAddr(nodehash, addr);
+      }
+
+      // Now transfer full ownership to the expected owner
+      base.reclaim(tokenId, msg.sender);
+      base.transferFrom(address(this), msg.sender, tokenId);
+    } else {
+      require(addr == address(0));
+      base.register(tokenId, msg.sender);
+    }
+    emit NameRegistered(name, label, msg.sender);
+  }
+
+  // Checks if the name is available in the registry and not blocked.
+  function _available(string memory name) private view returns (bool) {
+    bytes32 label = keccak256(bytes(name));
+    uint256 tokenId = uint256(label);
+    return !denyList[tokenId] && base.available(tokenId);
+  }
+
+  /**
+    Name generation
+  */
+
+  // Generates a pseudo-random name from 00000 to 99999.
+  function _generateName(uint256 attempt) public view returns (string memory) {
+    uint256 pseudorandomness = uint256(
+      keccak256(
+        abi.encodePacked(blockhash(block.number - 1), msg.sender, attempt)
+      )
+    );
+
+    uint256 max = 10**maxDigits;
+    uint16 number = uint16(pseudorandomness % max);
+    return _formatName(number);
+  }
+
+  // Formats the given number as a 5 digit 0-left-padded string.
+  function _formatName(uint16 v) private view returns (string memory) {
+    bytes memory b = new bytes(maxDigits);
+    uint256 digit = 0;
+    while (digit < maxDigits) {
+      b[maxDigits - digit - 1] = SYMBOLS[v % 10];
+      v /= 10;
+      digit++;
+    }
+    return string(b);
+  }
+
+  /** Claming */
+
+  // _consumeClaim consumes one non-expired claim or rejects the transaction.
+  function _consumeClaim() private {
+    (uint64 idx, bool found) = _findNonExpiredClaim();
+    require(found, 'no claims available');
+
+    delete claims[msg.sender][idx];
+  }
+
+  // returns the index of the first non-expired claim for the sender and true or (0, false) when not found.
+  function _findNonExpiredClaim() private view returns (uint64, bool) {
+    uint256[] memory list = claims[msg.sender];
+    for (uint64 i = 0; i < list.length; i++) {
+      uint256 expiry = list[i];
+      if (_isClaimValid(expiry, block.timestamp)) {
+        return (i, true);
+      }
+    }
+    return (0, false);
+  }
+
+  // returns the number of valid claims for the given address.
+  function claimsCount(address owner, uint256 currentTimestamp)
+    external
+    view
+    returns (uint64)
+  {
+    uint64 count = 0;
+    uint256[] memory list = claims[owner];
+    for (uint64 i = 0; i < list.length; i++) {
+      uint256 expiry = list[i];
+      if (_isClaimValid(expiry, currentTimestamp)) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  // _isClaimValid returns true is the given claim is valid.
+  function _isClaimValid(uint256 expiry, uint256 currentTimestamp)
+    private
+    pure
+    returns (bool)
+  {
+    return expiry > currentTimestamp;
+  }
+
+  /** ADMIN */
+
+  // Adds the given labels to the deny list.
+  function addToDenyList(uint256[] calldata tokenIds) external onlyOwner {
+    for (uint256 i = 0; i < tokenIds.length; i++) {
+      denyList[tokenIds[i]] = true;
+    }
+  }
+
+  // Removes the given labels to the deny list.
+  function removeFromDenyList(uint256[] calldata tokenIds) external onlyOwner {
+    for (uint256 i = 0; i < tokenIds.length; i++) {
+      delete denyList[tokenIds[i]];
+    }
+  }
+
+  // Removes the given labels to the deny list.
+  function createClaims(address[] calldata owners, uint64 expiry)
+    external
+    onlyOwner
+  {
+    for (uint256 i = 0; i < owners.length; i++) {
+      claims[owners[i]].push(expiry);
+    }
+  }
 }
---
+++ packages/contracts/test/ethregistrar/TestTenKClubController.js
@@ -0,0 +1,249 @@
+const ENS = artifacts.require('./registry/ENSRegistry')
+const BaseRegistrar = artifacts.require(
+  './registrar/BaseRegistrarImplementation',
+)
+const PublicResolver = artifacts.require('./resolvers/PublicResolver')
+const Controller = artifacts.require('./ethregistrar/TenKClubController')
+
+const namehash = require('eth-ens-namehash')
+const sha3 = require('web3-utils').sha3
+
+const { exceptions } = require('../test-utils')
+
+const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+const TLD = '⌐◨-◨'
+
+contract('TenKClubController', function (accounts) {
+  const [owner, w1, w2, w3] = accounts
+
+  let ens
+  let registrar
+  let controller
+  let resolver
+  const ALL_NAMES = Array.from({ length: 100 }).map((_, n) =>
+    String(n).padStart(2, '0'),
+  )
+
+  beforeEach(async () => {
+    ens = await ENS.new()
+    registrar = await BaseRegistrar.new(ens.address, namehash.hash(TLD), {
+      from: owner,
+    })
+    await ens.setSubnodeOwner('0x0', sha3(TLD), registrar.address)
+
+    controller = await Controller.new(registrar.address, 2)
+    await registrar.addController(controller.address, { from: owner })
+    await registrar.addController(owner, { from: owner })
+
+    resolver = await PublicResolver.new(ens.address, ZERO_ADDRESS, ZERO_ADDRESS)
+  })
+
+  describe('claims mgmt', () => {
+    it('returns zero as initial claims count', async () => {
+      const count = await controller.claimsCount(w1, Date.now())
+      assert.equal(count, 0)
+    })
+
+    it('should not allow setting claims by non-owner', async () => {
+      const op = controller.createClaims([], Date.now(), { from: w1 })
+      await exceptions.expectFailure(op)
+    })
+
+    it('should create claims', async () => {
+      const now = Math.round(Date.now() / 1000)
+      await controller.createClaims([w1, w2], now + 1e2, {
+        from: owner,
+      })
+      await controller.createClaims([w2], now + 1e4, {
+        from: owner,
+      })
+
+      const beforeFirstExpiry = now
+      assert.equal(await controller.claimsCount(w1, beforeFirstExpiry), 1)
+      assert.equal(await controller.claimsCount(w2, beforeFirstExpiry), 2)
+      assert.equal(await controller.claimsCount(w3, beforeFirstExpiry), 0)
+      assert.equal(await controller.claimsCount(owner, beforeFirstExpiry), 0)
+
+      const inBetweenExpiry = now + 1e3
+      assert.equal(await controller.claimsCount(w1, inBetweenExpiry), 0)
+      assert.equal(await controller.claimsCount(w2, inBetweenExpiry), 1)
+
+      const afterBothExpiry = now + 1e5
+      assert.equal(await controller.claimsCount(w1, afterBothExpiry), 0)
+      assert.equal(await controller.claimsCount(w2, afterBothExpiry), 0)
+    })
+  })
+
+  describe('registration', () => {
+    it('should not register if there are no claims', async () => {
+      const op = controller.register(ZERO_ADDRESS, w3)
+      await exceptions.expectFailure(op)
+    })
+
+    it('should register and consume a claim', async () => {
+      let { timestamp } = await web3.eth.getBlock('latest')
+      await controller.createClaims([w1], timestamp + 1e4, {
+        from: owner,
+      })
+      const claimsBefore = await controller.claimsCount(w1, timestamp)
+      assert.isAbove(Number(claimsBefore), 0)
+
+      const tx = await controller.register(resolver.address, w1, {
+        from: w1,
+      })
+      const event = tx.receipt.logs[0]
+      assert.equal(event.event, 'NameRegistered')
+      assert.isTrue(/^\d{2}$/.test(event.args.name))
+      assert.equal(event.args.owner, w1)
+      assert.equal(event.args.label, sha3(event.args.name))
+
+      const name = event.args.name
+      const tokenId = sha3(name)
+      const node = namehash.hash(`${name}.${TLD}`)
+
+      const claimsAfter = await controller.claimsCount(w1, timestamp)
+      assert.equal(claimsAfter, claimsBefore - 1)
+
+      // Check ownership on registrar and registry
+      assert.equal(await registrar.ownerOf(tokenId), w1)
+      assert.equal(await ens.owner(node), w1)
+      // Check resolver is set
+      assert.equal(await ens.resolver(node), resolver.address)
+      // Check value of the address is set on the resolver
+      assert.equal(await resolver.addr(node), w1)
+    })
+
+    it('should try until one is available in the registrar', async () => {
+      const target = '88'
+      // Register all names but the target.
+      for (const n of ALL_NAMES) {
+        if (n === target) {
+          continue
+        }
+        await registrar.register(sha3(n), w3, { from: owner }).catch(() => {}) // ignore if already registered
+      }
+
+      let { timestamp } = await web3.eth.getBlock('latest')
+      await controller.createClaims([w1], timestamp + 1e4, {
+        from: owner,
+      })
+      const tx = await controller.register(resolver.address, w1, {
+        from: w1,
+      })
+      const event = tx.receipt.logs[0]
+      const name = event.args.name
+      assert.equal(name, target)
+    })
+
+    it('should try until one is not in the deny list', async () => {
+      const target = '88'
+      // Register all names but the target.
+      const labels = ALL_NAMES.filter((n) => n !== target).map((n) => sha3(n))
+      await controller.addToDenyList(labels, {
+        from: owner,
+      })
+
+      let { timestamp } = await web3.eth.getBlock('latest')
+      await controller.createClaims([w1], timestamp + 1e4, {
+        from: owner,
+      })
+      const tx = await controller.register(resolver.address, w1, {
+        from: w1,
+      })
+      const event = tx.receipt.logs[0]
+      const name = event.args.name
+      assert.equal(name, target)
+    })
+
+    it('should sample and register a 4 digit name', async () => {
+      const controller = await Controller.new(registrar.address, 4)
+      await registrar.addController(controller.address, { from: owner })
+
+      let { timestamp } = await web3.eth.getBlock('latest')
+      await controller.createClaims([w1], timestamp + 1e4, {
+        from: owner,
+      })
+      const tx = await controller.register(resolver.address, w1, {
+        from: w1,
+      })
+
+      const event = tx.receipt.logs[0]
+      assert.isTrue(/^\d{4}$/.test(event.args.name))
+    })
+  })
+
+  describe('deny list', () => {
+    it('should not allow non-owner to add to the deny list', async () => {
+      const op = controller.addToDenyList([sha3('test')], {
+        from: w1,
+      })
+      await exceptions.expectFailure(op)
+    })
+
+    it('should not allow non-owner to remove from the deny list', async () => {
+      const op = controller.removeFromDenyList([sha3('test')], {
+        from: w1,
+      })
+      await exceptions.expectFailure(op)
+    })
+
+    it('should add and remove from the list', async () => {
+      const n1 = sha3('block-1')
+      const n2 = sha3('block-2')
+
+      // Add and check
+      await controller.addToDenyList([n1, n2], {
+        from: owner,
+      })
+      assert.isTrue(await controller.denyList(n1))
+      assert.isTrue(await controller.denyList(n2))
+
+      // Remove one and check
+      await controller.removeFromDenyList([n1], {
+        from: owner,
+      })
+      assert.isFalse(await controller.denyList(n1))
+      assert.isTrue(await controller.denyList(n2))
+
+      // Remove the other one and check
+      await controller.removeFromDenyList([n2], {
+        from: owner,
+      })
+      assert.isFalse(await controller.denyList(n1))
+      assert.isFalse(await controller.denyList(n2))
+    })
+  })
+
+  describe('name generation', () => {
+    let names
+
+    before(async () => {
+      names = await Promise.all(
+        ALL_NAMES.map((v, idx) => {
+          return controller._generateName(idx)
+        }),
+      )
+    })
+
+    it('should not sample the same name too many times', async () => {
+      const counters = names.reduce(
+        (acc, cur) => ({ ...acc, [cur]: (acc[cur] || 0) + 1 }),
+        {},
+      )
+      const threshold = 5
+      for (const [name, count] of Object.entries(counters)) {
+        assert.isBelow(
+          count,
+          threshold,
+          `'${name}' sampled more than ${threshold} times`,
+        )
+      }
+    })
+
+    it('should only sample 2 digit numbers', async () => {
+      for (const name of names) {
+        assert.isTrue(/^\d{2}$/.test(name), `${name} is not a 2 digit number`)
+      }
+    })
+  })
+})
