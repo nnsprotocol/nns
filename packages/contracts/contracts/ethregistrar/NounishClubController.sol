250da045b4eb81e33a64b27c81793e867dc75a1b
apbigcod <apbigcod@proton.me>
2023-03-12 18:58:47 +0000

    new controller with signature

--- packages/contracts/contracts/ethregistrar/NounishClubController.sol
+++ packages/contracts/contracts/ethregistrar/NounishClubController.sol
@@ -5,46 +5,51 @@ import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/utils/Strings.sol';
 import '../resolvers/Resolver.sol';
 import './BaseRegistrarImplementation.sol';
+import '@openzeppelin/contracts/utils/cryptography/ECDSA.sol';
+import 'hardhat/console.sol';
 
 // NounishClubController registers a random name from 0 to 9999
 // for accounts that have one or more claims available.
 contract NounishClubController is Ownable {
+  using ECDSA for bytes32;
+
   event NameRegistered(
     string name,
     bytes32 indexed label,
     address indexed owner
   );
 
-  bytes16 private constant SYMBOLS = '0123456789';
-
-  mapping(uint256 => bool) public denyList;
-  // Map of expiry timestamps for claims associated to addresses.
-  mapping(address => uint256[]) private claims;
   BaseRegistrarImplementation base;
-  uint256 private minValue;
-  uint256 private maxValue;
+  address private signer;
+  mapping(address => mapping(uint256 => bool)) usedNonces;
 
-  uint256 private namesRemaining;
-  mapping(uint256 => uint256) movedNames;
-
-  constructor(
-    BaseRegistrarImplementation _base,
-    uint256 _min,
-    uint256 _max
-  ) {
+  constructor(BaseRegistrarImplementation _base, address _signer) {
     base = _base;
-    minValue = _min;
-    maxValue = _max;
-    namesRemaining = _max - _min + 1;
+    signer = _signer;
   }
 
-  /**
-    Registration
-  */
+  function register(
+    uint16 number,
+    uint256 nonce,
+    uint256 expiry,
+    address resolver,
+    address addr,
+    bytes memory signature
+  ) public {
+    require(!usedNonces[msg.sender][nonce], 'nonce already used');
 
-  // Register a random name from min to max.
-  function register(address resolver, address addr) external {
-    _register(Strings.toString(_draw()), resolver, addr);
+    bytes32 hash = keccak256(
+      abi.encodePacked(msg.sender, number, nonce, expiry, resolver, addr)
+    );
+    address msgSigner = hash.toEthSignedMessageHash().recover(signature);
+    require(msgSigner == signer, 'invalid signature');
+
+    require(expiry > block.timestamp, 'expired');
+
+    string memory name = Strings.toString(number);
+
+    usedNonces[msg.sender][nonce] = true;
+    _register(name, resolver, addr);
   }
 
   // Register the given name.
@@ -54,7 +59,6 @@ contract NounishClubController is Ownable {
     address addr
   ) private {
     require(_available(name), 'name not available');
-    _consumeClaim();
 
     bytes32 label = keccak256(bytes(name));
     uint256 tokenId = uint256(label);
@@ -90,114 +94,6 @@ contract NounishClubController is Ownable {
   function _available(string memory name) private view returns (bool) {
     bytes32 label = keccak256(bytes(name));
     uint256 tokenId = uint256(label);
-    return !denyList[tokenId] && base.available(tokenId);
-  }
-
-  /**
-    Name generation
-  */
-  function _nameAt(uint256 i) private view returns (uint256) {
-    if (movedNames[i] > 0) {
-      return movedNames[i];
-    } else {
-      return i;
-    }
-  }
-
-  // draws the next number.
-  function _draw() private returns (uint256) {
-    require(namesRemaining > 0, 'No names left');
-
-    uint256 random = uint256(
-      keccak256(abi.encodePacked(blockhash(block.number - 1), msg.sender))
-    );
-    uint16 i = uint16(random % namesRemaining);
-    uint256 pickedName = _nameAt(i) + minValue;
-
-    // Move the last card in the deck into position i
-    movedNames[i] = _nameAt(namesRemaining - 1);
-    movedNames[namesRemaining - 1] = 0;
-    namesRemaining -= 1;
-
-    return pickedName;
-  }
-
-  /** Claming */
-
-  // _consumeClaim consumes one non-expired claim or rejects the transaction.
-  function _consumeClaim() private {
-    (uint64 idx, bool found) = _findNonExpiredClaim();
-    require(found, 'no claims available');
-
-    delete claims[msg.sender][idx];
-  }
-
-  // returns the index of the first non-expired claim for the sender and true or (0, false) when not found.
-  function _findNonExpiredClaim() private view returns (uint64, bool) {
-    uint256[] memory list = claims[msg.sender];
-    for (uint64 i = 0; i < list.length; i++) {
-      uint256 expiry = list[i];
-      if (_isClaimValid(expiry, block.timestamp)) {
-        return (i, true);
-      }
-    }
-    return (0, false);
-  }
-
-  // returns the number of valid claims for the given address.
-  function claimsCount(address owner, uint256 currentTimestamp)
-    external
-    view
-    returns (uint64)
-  {
-    uint64 count = 0;
-    uint256[] memory list = claims[owner];
-    for (uint64 i = 0; i < list.length; i++) {
-      uint256 expiry = list[i];
-      if (_isClaimValid(expiry, currentTimestamp)) {
-        count++;
-      }
-    }
-    return count;
-  }
-
-  // _isClaimValid returns true is the given claim is valid.
-  function _isClaimValid(uint256 expiry, uint256 currentTimestamp)
-    private
-    pure
-    returns (bool)
-  {
-    return expiry > currentTimestamp;
-  }
-
-  /** ADMIN */
-
-  // Adds the given labels to the deny list.
-  function addToDenyList(uint256[] calldata tokenIds) external onlyOwner {
-    for (uint256 i = 0; i < tokenIds.length; i++) {
-      require(base.available(tokenIds[i]), 'name must be available');
-      if (!denyList[tokenIds[i]]) {
-        denyList[tokenIds[i]] = true;
-      }
-    }
-  }
-
-  // Removes the given labels to the deny list.
-  function removeFromDenyList(uint256[] calldata tokenIds) external onlyOwner {
-    for (uint256 i = 0; i < tokenIds.length; i++) {
-      if (denyList[tokenIds[i]]) {
-        delete denyList[tokenIds[i]];
-      }
-    }
-  }
-
-  // Removes the given labels to the deny list.
-  function createClaims(address[] calldata owners, uint64 expiry)
-    external
-    onlyOwner
-  {
-    for (uint256 i = 0; i < owners.length; i++) {
-      claims[owners[i]].push(expiry);
-    }
+    return base.available(tokenId);
   }
 }
--- packages/contracts/test/ethregistrar/NounishClubController.js
+++ packages/contracts/test/ethregistrar/NounishClubController.js
@@ -4,6 +4,7 @@ const BaseRegistrar = artifacts.require(
 )
 const PublicResolver = artifacts.require('./resolvers/PublicResolver')
 const Controller = artifacts.require('./ethregistrar/NounishClubController')
+const { ethers } = require('hardhat')
 
 const namehash = require('eth-ens-namehash')
 const sha3 = require('web3-utils').sha3
@@ -13,226 +14,286 @@ const { exceptions } = require('../test-utils')
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 const TLD = '⌐◨-◨'
 
-contract('NounishClubController', function (accounts) {
-  const [owner, w1, w2, w3] = accounts
-
-  const FROM = 1003
-  const TO = 1094
-
+contract('NounishClubController', function () {
   let ens
   let registrar
   let controller
   let resolver
-  const ALL_NAMES = Array.from({ length: TO - FROM + 1 }).map((_, n) =>
-    String(FROM + n),
-  )
+  let owner
+  let signer
+  let w1, w2
+  let wallets
 
   beforeEach(async () => {
+    const accounts = await ethers.getSigners()
+    owner = accounts[0]
+    signer = accounts[1]
+    w1 = accounts[2]
+    w2 = accounts[3]
+    wallets = {
+      w1,
+      w1,
+      owner,
+      signer,
+    }
     ens = await ENS.new()
     registrar = await BaseRegistrar.new(ens.address, namehash.hash(TLD), {
-      from: owner,
+      from: owner.address,
     })
     await ens.setSubnodeOwner('0x0', sha3(TLD), registrar.address)
 
-    controller = await Controller.new(registrar.address, FROM, TO)
-    await registrar.addController(controller.address, { from: owner })
-    await registrar.addController(owner, { from: owner })
+    controller = await Controller.new(registrar.address, signer.address)
+    await registrar.addController(controller.address, { from: owner.address })
+    await registrar.addController(owner.address, { from: owner.address })
 
     resolver = await PublicResolver.new(ens.address, ZERO_ADDRESS, ZERO_ADDRESS)
   })
 
-  describe('claims mgmt', () => {
-    it('returns zero as initial claims count', async () => {
-      const count = await controller.claimsCount(w1, Date.now())
-      assert.equal(count, 0)
-    })
+  async function createSignature(signer, data) {
+    const hash = ethers.utils.solidityKeccak256(
+      // sender, number, nonce, expiry, resolver, addr
+      ['address', 'uint16', 'uint256', 'uint256', 'address', 'address'],
+      [
+        data.sender,
+        data.number,
+        data.nonce,
+        data.expiry,
+        data.resolver,
+        data.addr,
+      ],
+    )
+    const signature = await signer.signMessage(ethers.utils.arrayify(hash))
+    return {
+      ...data,
+      signature,
+    }
+  }
 
-    it('should not allow setting claims by non-owner', async () => {
-      const op = controller.createClaims([], Date.now(), { from: w1 })
-      await exceptions.expectFailure(op)
-    })
-
-    it('should create claims', async () => {
-      const now = Math.round(Date.now() / 1000)
-      await controller.createClaims([w1, w2], now + 1e2, {
-        from: owner,
-      })
-      await controller.createClaims([w2], now + 1e4, {
-        from: owner,
-      })
-
-      const beforeFirstExpiry = now
-      assert.equal(await controller.claimsCount(w1, beforeFirstExpiry), 1)
-      assert.equal(await controller.claimsCount(w2, beforeFirstExpiry), 2)
-      assert.equal(await controller.claimsCount(w3, beforeFirstExpiry), 0)
-      assert.equal(await controller.claimsCount(owner, beforeFirstExpiry), 0)
-
-      const inBetweenExpiry = now + 1e3
-      assert.equal(await controller.claimsCount(w1, inBetweenExpiry), 0)
-      assert.equal(await controller.claimsCount(w2, inBetweenExpiry), 1)
-
-      const afterBothExpiry = now + 1e5
-      assert.equal(await controller.claimsCount(w1, afterBothExpiry), 0)
-      assert.equal(await controller.claimsCount(w2, afterBothExpiry), 0)
-    })
+  const validData = () => ({
+    sender: w1.address,
+    number: 1234,
+    nonce: 9999999,
+    expiry: Date.now() + 1000,
+    resolver: resolver.address,
+    addr: w1.address,
   })
 
-  describe('registration', () => {
-    it('should not register if there are no claims', async () => {
-      const op = controller.register(ZERO_ADDRESS, w3)
-      await exceptions.expectFailure(op)
-    })
-
-    it('should register and consume a claim', async () => {
-      let { timestamp } = await web3.eth.getBlock('latest')
-      await controller.createClaims([w1], timestamp + 1e4, {
-        from: owner,
-      })
-      const claimsBefore = await controller.claimsCount(w1, timestamp)
-      assert.isAbove(Number(claimsBefore), 0)
-
-      const tx = await controller.register(resolver.address, w1, {
-        from: w1,
-      })
-      const event = tx.receipt.logs[0]
-      assert.equal(event.event, 'NameRegistered')
-      assert.isNotNaN(parseInt(event.args.name))
-      assert.equal(event.args.owner, w1)
-      assert.equal(event.args.label, sha3(event.args.name))
-
-      const name = event.args.name
-      const tokenId = sha3(name)
-      const node = namehash.hash(`${name}.${TLD}`)
-
-      const claimsAfter = await controller.claimsCount(w1, timestamp)
-      assert.equal(claimsAfter, claimsBefore - 1)
-
-      // Check ownership on registrar and registry
-      assert.equal(await registrar.ownerOf(tokenId), w1)
-      assert.equal(await ens.owner(node), w1)
-      // Check resolver is set
-      assert.equal(await ens.resolver(node), resolver.address)
-      // Check value of the address is set on the resolver
-      assert.equal(await resolver.addr(node), w1)
-    })
-
-    it('should reject when all names are registered', async () => {
-      for (const n of ALL_NAMES) {
-        await registrar.register(sha3(n), w3, { from: owner })
+  const tests = [
+    {
+      name: 'signed by other wallet',
+      context: () => ({
+        signer: w2,
+        sender: w1,
+        data: validData(),
+      }),
+    },
+    {
+      name: 'number has changed',
+      context: () => ({
+        signer: signer,
+        sender: w1,
+        data: validData(),
+        update: (d) => {
+          d.number = 666
+        },
+      }),
+    },
+    {
+      name: 'expiry has changed',
+      context: () => ({
+        signer: signer,
+        sender: w1,
+        data: validData(),
+        update: (d) => {
+          d.expiry = Date.now() + 123 + 1000
+        },
+      }),
+    },
+    {
+      name: 'nonce has changed',
+      context: () => ({
+        signer: signer,
+        sender: w1,
+        data: validData(),
+        update: (d) => {
+          d.nonce = 998822334455
+        },
+      }),
+    },
+    {
+      name: 'resolver has changed',
+      context: () => ({
+        signer: signer,
+        sender: w1,
+        data: validData(),
+        update: (d) => {
+          d.resolver = owner.address
+        },
+      }),
+    },
+    {
+      name: 'addr has changed',
+      context: () => ({
+        signer: signer,
+        sender: w1,
+        data: validData(),
+        update: (d) => {
+          d.addr = owner.address
+        },
+      }),
+    },
+    {
+      name: 'bad signature',
+      context: () => ({
+        signer: signer,
+        sender: w1,
+        data: validData(),
+        update: (d) => {
+          d.signature = '0xAABB' + d.signature.slice(6)
+        },
+      }),
+    },
+    {
+      name: 'wrong sender',
+      context: () => ({
+        signer: signer,
+        sender: w1,
+        data: {
+          ...validData(),
+          sender: w2.address,
+        },
+      }),
+    },
+    {
+      name: 'expired',
+      context: () => ({
+        signer: signer,
+        sender: w1,
+        data: {
+          ...validData(),
+          expiry: 100,
+        },
+      }),
+    },
+  ].forEach((t) => {
+    it(t.name, async () => {
+      const ctx = t.context()
+      const data = await createSignature(ctx.signer, ctx.data)
+      if (ctx.update) {
+        ctx.update(data)
       }
-
-      let { timestamp } = await web3.eth.getBlock('latest')
-      await controller.createClaims([w1], timestamp + 1e4, {
-        from: owner,
-      })
-      const op = controller.register(resolver.address, w1, {
-        from: w1,
-      })
-      await exceptions.expectFailure(op)
-    })
-
-    it('should reject when all names are in the deny list', async () => {
-      const labels = ALL_NAMES.map((n) => sha3(n))
-      await controller.addToDenyList(labels, {
-        from: owner,
-      })
-
-      let { timestamp } = await web3.eth.getBlock('latest')
-      await controller.createClaims([w1], timestamp + 1e4, {
-        from: owner,
-      })
-      const op = controller.register(resolver.address, w1, {
-        from: w1,
-      })
-      await exceptions.expectFailure(op)
-    })
-
-    it('should sample and register a name between FROM and TO', async () => {
-      let { timestamp } = await web3.eth.getBlock('latest')
-      await controller.createClaims([w1], timestamp + 1e4, {
-        from: owner,
-      })
-      const tx = await controller.register(resolver.address, w1, {
-        from: w1,
-      })
-
-      const event = tx.receipt.logs[0]
-      const num = parseInt(event.args.name)
-      assert.isAtLeast(num, FROM)
-      assert.isAtMost(num, TO)
-    })
-
-    it('should register all names from FROM to TO', async () => {
-      const { timestamp } = await web3.eth.getBlock('latest')
-      for (let i = FROM; i <= TO; i++) {
-        await controller.createClaims([w1], timestamp + 1e4, {
-          from: owner,
-        })
-      }
-      const registeredNumbers = new Set()
-      for (let i = FROM; i <= TO; i++) {
-        const tx = await controller.register(resolver.address, w1, {
-          from: w1,
-        })
-        const event = tx.receipt.logs[0]
-        const num = parseInt(event.args.name)
-        assert.isFalse(registeredNumbers.has(num))
-        assert.isAtLeast(num, FROM)
-        assert.isAtMost(num, TO)
-        registeredNumbers.add(num)
-      }
-      assert.lengthOf(registeredNumbers, TO - FROM + 1)
-      assert.equal(0, await controller.claimsCount(w1, timestamp + 1e4))
-      // Next should fail
-      await controller.createClaims([w1], timestamp + 1e4, {
-        from: owner,
-      })
-      const op = controller.register(resolver.address, w1, {
-        from: w1,
-      })
+      const op = controller.register(
+        data.number,
+        data.nonce,
+        data.expiry,
+        data.resolver,
+        data.addr,
+        data.signature,
+        { from: ctx.sender.address },
+      )
       await exceptions.expectFailure(op)
     })
   })
 
-  describe('deny list', () => {
-    it('should not allow non-owner to add to the deny list', async () => {
-      const op = controller.addToDenyList([sha3('test')], {
-        from: w1,
-      })
-      await exceptions.expectFailure(op)
+  it('name not available', async () => {
+    const number = 9988
+    await registrar.register(sha3(`${number}`), owner.address, {
+      from: owner.address,
     })
 
-    it('should not allow non-owner to remove from the deny list', async () => {
-      const op = controller.removeFromDenyList([sha3('test')], {
-        from: w1,
-      })
-      await exceptions.expectFailure(op)
+    const data = await createSignature(signer, {
+      ...validData(),
+      number,
     })
+    const op = controller.register(
+      data.number,
+      data.nonce,
+      data.expiry,
+      data.resolver,
+      data.addr,
+      data.signature,
+      { from: data.sender },
+    )
+    await exceptions.expectFailure(op)
+  })
 
-    it('should add and remove from the list', async () => {
-      const n1 = sha3('block-1')
-      const n2 = sha3('block-2')
-
-      // Add and check
-      await controller.addToDenyList([n1, n2], {
-        from: owner,
-      })
-      assert.isTrue(await controller.denyList(n1))
-      assert.isTrue(await controller.denyList(n2))
-
-      // Remove one and check
-      await controller.removeFromDenyList([n1], {
-        from: owner,
-      })
-      assert.isFalse(await controller.denyList(n1))
-      assert.isTrue(await controller.denyList(n2))
-
-      // Remove the other one and check
-      await controller.removeFromDenyList([n2], {
-        from: owner,
-      })
-      assert.isFalse(await controller.denyList(n1))
-      assert.isFalse(await controller.denyList(n2))
+  it('nonce already used', async () => {
+    // register first name successfully
+    const nonce = 2
+    let data = await createSignature(signer, {
+      ...validData(),
+      nonce,
+      number: 7,
     })
+    await controller.register(
+      data.number,
+      data.nonce,
+      data.expiry,
+      data.resolver,
+      data.addr,
+      data.signature,
+      { from: data.sender },
+    )
+
+    // Register a second number with the same nonce
+    data = await createSignature(signer, {
+      ...validData(),
+      nonce,
+      number: 8,
+    })
+    const op = controller.register(
+      data.number,
+      data.nonce,
+      data.expiry,
+      data.resolver,
+      data.addr,
+      data.signature,
+      { from: data.sender },
+    )
+    await exceptions.expectFailure(op)
+  })
+
+  it('fails when registering twice', async () => {
+    // register first name successfully
+
+    let data = await createSignature(signer, validData())
+    await controller.register(
+      data.number,
+      data.nonce,
+      data.expiry,
+      data.resolver,
+      data.addr,
+      data.signature,
+      { from: data.sender },
+    )
+    const op = controller.register(
+      data.number,
+      data.nonce,
+      data.expiry,
+      data.resolver,
+      data.addr,
+      data.signature,
+      { from: data.sender },
+    )
+    await exceptions.expectFailure(op)
+  })
+
+  it('successfully registers number', async () => {
+    const data = await createSignature(signer, validData())
+    const tx = await controller.register(
+      data.number,
+      data.nonce,
+      data.expiry,
+      data.resolver,
+      data.addr,
+      data.signature,
+      { from: data.sender },
+    )
+
+    const event = tx.receipt.logs[0]
+    assert.equal(event.event, 'NameRegistered')
+    assert.equal(parseInt(event.args.name), data.number.toString())
+    assert.equal(event.args.owner, data.sender)
+    assert.equal(event.args.label, sha3(event.args.name))
   })
 })
