8a2d117629c6c36f53e30b47e1460dc3b8c3c2ca
apbigcod <apbigcod@proton.me>
2024-09-20 15:12:40 +0200

    benefit sections

--- packages/webapp/src/components/collection-details/SectionAbout.tsx
+++ packages/webapp/src/components/collection-details/SectionAbout.tsx
@@ -1,6 +1,5 @@
-import { useRegistry } from "../../services/graph";
-import GroupSocialLinks from "../ui/groups/GroupSocialLinks";
 import { CollectionData } from "../../services/collections";
+import { useRegistry } from "../../services/graph";
 
 interface SectionAboutProps {
   collection: CollectionData;
@@ -12,18 +11,22 @@ const SectionAbout: React.FC<SectionAboutProps> = ({ collection }) => {
   });
 
   return (
-    <section className="w-full lg:max-w-[1200px] border border-borderPrimary rounded-32 bg-cardSurfaceGradient p-lg m-auto">
-      <div className="flex justify-between">
-        <p className="text-xs font-medium text-textInverse">
-          Why you need a .{collection.cld} name
-        </p>
-        <GroupSocialLinks collection={collection} />
-      </div>
-      <div className="grid grid-cols-1 gap-lg pb-lg">
+    <div className="lg:max-w-[50%] p-lg border border-borderPrimary rounded-32 relative bg-cardPrimaryGradient">
+      <div className="absolute inset-0 backdrop-blur-[8px] rounded-32 z-0"></div>
+      <div className="relative z-10">
+        <div className="flex items-center justify-between gap-xs mb-xl">
+          <p className="text-xs font-medium text-textInverse uppercase">
+            About
+          </p>
+          {/* <GroupSocialLinks
+                  customLinkClassName="button-md py-0 px-1 flex items-center justify-center"
+                  iconSize={16}
+                /> */}
+        </div>
         <div className="grid grid-cols-1 gap-md">
           <div>
             <div className="my-6 flex justify-center">
-              <img src={collection.logoSrc} width={100} height={100} alt="" />
+              <img src={collection.logoSrc} width={72} height={72} alt="" />
             </div>
             <p className="text-2xl font-semibold my-1 text-center">
               {collection.name}
@@ -32,8 +35,8 @@ const SectionAbout: React.FC<SectionAboutProps> = ({ collection }) => {
           <p className="text-base text-textSecondary text-center">
             {collection.description}
           </p>
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-md">
-            <div className="bg-surfaceSecondary rounded-2xl p-md flex flex-col items-center gap-sm justify-center">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-md bg-surfaceSecondary rounded-2xl p-md mt-xs">
+            <div className="flex flex-col items-start justify-center gap-sm">
               <p className="text-sm text-textSecondary font-medium">
                 Registered Names
               </p>
@@ -41,13 +44,13 @@ const SectionAbout: React.FC<SectionAboutProps> = ({ collection }) => {
                 {registry.data?.totalSupply}
               </p>
               {/* <div className="flex gap-xxs items-center">
-                <IconChevronUp />
-                <span className="text-xs text-textSecondary font-medium">
-                  2.45% Past Week
-                </span>
-              </div> */}
+                      <IconChevronUp />
+                      <span className="text-xs text-textSecondary font-medium">
+                        <span className="text-[#19BB46]">2.45% </span>Past Week
+                      </span>
+                    </div> */}
             </div>
-            <div className="bg-surfaceSecondary rounded-2xl p-md flex flex-col items-center gap-sm justify-center">
+            <div className="flex flex-col items-start justify-center gap-sm">
               <p className="text-sm text-textSecondary font-medium">
                 Unique Owners
               </p>
@@ -55,16 +58,16 @@ const SectionAbout: React.FC<SectionAboutProps> = ({ collection }) => {
                 {registry.data?.uniqueOwners}
               </p>
               {/* <div className="flex gap-xxs items-center">
-                <IconChevronUp />
-                <span className="text-xs text-textSecondary font-medium">
-                  1.08% Past Week
-                </span>
-              </div> */}
+                      <IconChevronUp />
+                      <span className="text-xs text-textSecondary font-medium">
+                        <span className="text-[#19BB46]">1.08% </span>Past Week
+                      </span>
+                    </div> */}
             </div>
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
