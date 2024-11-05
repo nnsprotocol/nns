fe79fad3fcb61308b414b14bca213dd82cfd02f1
apbigcod <apbigcod@proton.me>
2024-11-05 17:23:23 +0100

    new favicon

--- packages/webapp/vite.config.ts
+++ packages/webapp/vite.config.ts
@@ -1,11 +1,12 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import Unfonts from "unplugin-fonts/vite";
+import { defineConfig } from "vite";
+import { ViteFaviconsPlugin } from "vite-plugin-favicon";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    ViteFaviconsPlugin("./public/favicon.png"),
     react(),
     Unfonts({
       custom: {
